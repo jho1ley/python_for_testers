0929c7a06203945f9521bc15f705ab6b4a5670fc
jholley <129405368+jho1ley@users.noreply.github.com>
2024-02-27 21:36:37 +0700

    Add contact tests reworked with pytest (#2)

--- 1_infrustructure/application.py
+++ 1_infrustructure/application.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-
 
 class Application:
 
@@ -17,6 +14,10 @@ class Application:
     def logout(self):
         driver = self.driver
         driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    def return_home_page(self, driver):
+        driver = self.driver
+        driver.find_element(By.LINK_TEXT, "home page").click()
 
     def return_to_groups(self):
         driver = self.driver
@@ -38,6 +39,46 @@ class Application:
         # submit creation
         driver.find_element(By.NAME, "submit").click()
         self.return_to_groups()
+
+    def create_contact(self, contact):
+        driver = self.driver
+        # open contact form
+        driver.find_element(By.LINK_TEXT, "add new").click()
+        # adding fistname
+        driver.find_element(By.NAME, "firstname").click()
+        driver.find_element(By.NAME, "firstname").clear()
+        driver.find_element(By.NAME, "firstname").send_keys(contact.firstname)
+        # adding lastname
+        driver.find_element(By.NAME, "lastname").click()
+        driver.find_element(By.NAME, "lastname").clear()
+        driver.find_element(By.NAME, "lastname").send_keys(contact.lastname)
+        # adding nickname
+        driver.find_element(By.NAME, "nickname").click()
+        driver.find_element(By.NAME, "nickname").clear()
+        driver.find_element(By.NAME, "nickname").send_keys(contact.nickname)
+        # adding information about company
+        driver.find_element(By.NAME, "company").click()
+        driver.find_element(By.NAME, "company").clear()
+        driver.find_element(By.NAME, "company").send_keys(contact.company)
+        # adding information about address
+        driver.find_element(By.NAME, "address").click()
+        driver.find_element(By.NAME, "address").clear()
+        driver.find_element(By.NAME, "address").send_keys(contact.address)
+        # adding home number
+        driver.find_element(By.NAME, "home").click()
+        driver.find_element(By.NAME, "home").clear()
+        driver.find_element(By.NAME, "home").send_keys(contact.home)
+        # adding mobile number
+        driver.find_element(By.NAME, "mobile").click()
+        driver.find_element(By.NAME, "mobile").clear()
+        driver.find_element(By.NAME, "mobile").send_keys(contact.mobile)
+        # adding email address
+        driver.find_element(By.NAME, "email").click()
+        driver.find_element(By.NAME, "email").clear()
+        driver.find_element(By.NAME, "email").send_keys(contact.email)
+        # saving contact
+        driver.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
+        self.return_home_page(driver)
 
     def open_groups_page(self):
         driver = self.driver
