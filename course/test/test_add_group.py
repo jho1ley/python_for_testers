544e2eb8560fb17d579b084e254292150f9b0b7a
jho1ley <a.voshchinkin@gmail.com>
2024-03-03 20:26:55 +0700

    include fix

--- course/test/test_add_group.py
+++ course/test/test_add_group.py
@@ -11,10 +11,10 @@ def app(request):
 
 def test_create_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create_group(Group(name="zalupa", header="asdf", footer="fefef"))
+    app.group.create(Group(name="zalupa", header="asdf", footer="fefef"))
     app.session.logout()
 
 def test_create_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
